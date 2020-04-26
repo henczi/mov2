cf50a2e7f8b4b95863ab634346ebfd00bb3480a4
Henczidai Bálint <henczidaibalint@gmail.com>
2020-04-26 14:03:23 +0200

    mock videos

--- core/service/mock/index.js
+++ core/service/mock/index.js
@@ -1,3 +1,123 @@
+const sampleVideos = [
+  {
+    "description": "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    "userName": "By Blender Foundation",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
+    "title": "Big Buck Bunny",
+    "uploadTime": "2 napja",
+    "duration": "9:56"
+  },
+  {
+    "description": "The first Blender Open Movie from 2006",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    "userName": "By Blender Foundation",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
+    "title": "Elephant Dream",
+    "uploadTime": "2 napja",
+    "duration": "6:43"
+  },
+  {
+    "description": "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    "userName": "By Google",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerBlazes.jpg",
+    "title": "For Bigger Blazes",
+    "uploadTime": "2 napja",
+    "duration": "3:21"
+  },
+  {
+    "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with Google Play Movies and more at google.com/chromecast.",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+    "userName": "By Google",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerEscapes.jpg",
+    "title": "For Bigger Escape",
+    "uploadTime": "2 napja",
+    "duration": "7:13"
+  },
+  {
+    "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35.  Find out more at google.com/chromecast.",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+    "userName": "By Google",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerFun.jpg",
+    "title": "For Bigger Fun",
+    "uploadTime": "2 napja",
+    "duration": "11:51"
+  },
+  {
+    "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for the times that call for bigger joyrides. For $35. Learn how to use Chromecast with YouTube and more at google.com/chromecast.",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+    "userName": "By Google",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerJoyrides.jpg",
+    "title": "For Bigger Joyrides",
+    "uploadTime": "2 napja",
+    "duration": "1:41"
+  },
+  {
+    "description": "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use Chromecast with Netflix and more at google.com/chromecast.",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+    "userName": "By Google",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg",
+    "title": "For Bigger Meltdowns",
+    "uploadTime": "2 napja",
+    "duration": "5:16"
+  },
+  {
+    "description": "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+    "userName": "By Blender Foundation",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg",
+    "title": "Sintel",
+    "uploadTime": "2 napja",
+    "duration": "7:59"
+  },
+  {
+    "description": "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+    "userName": "By Garage419",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/SubaruOutbackOnStreetAndDirt.jpg",
+    "title": "Subaru Outback On Street And Dirt",
+    "uploadTime": "2 napja",
+    "duration": "3:10"
+  },
+  {
+    "description": "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+    "userName": "By Blender Foundation",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg",
+    "title": "Tears of Steel",
+    "uploadTime": "2 napja",
+    "duration": "12:44"
+  },
+  {
+    "description": "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4",
+    "userName": "By Garage419",
+    "image": "https://source.unsplash.com/random/300x200",
+    "title": "Volkswagen GTI Review",
+    "uploadTime": "2 napja",
+    "duration": "22:29"
+  },
+  {
+    "description": "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+    "userName": "By Garage419",
+    "image": "https://source.unsplash.com/random/300x200",
+    "title": "We Are Going On Bullrun",
+    "uploadTime": "2 napja",
+    "duration": "2:47"
+  },
+  {
+    "description": "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
+    "image": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4",
+    "userName": "By Garage419",
+    "image": "https://source.unsplash.com/random/300x200",
+    "title": "What care can you get for a grand?",
+    "uploadTime": "2 napja",
+    "duration": "1:02"
+  }
+]
+
 const pageSize = 10;
 
 async function search(term, page = 1) {
@@ -5,19 +125,20 @@ async function search(term, page = 1) {
   const totalCount = 5 + term.length * term.length;
   let remaining = totalCount - ((page - 1) * pageSize);
   let nextPage = remaining > pageSize ? (page + 1) : undefined;
-  Array(remaining > pageSize ? pageSize : remaining).fill(0)
+  const generatedItems = Array(remaining > pageSize ? pageSize : remaining).fill(0).map((x, i) => ({
+    title: 'Mock' + term + ' cím ' + (i + page * pageSize + 1),
+    href: '',
+    duration: (i + page * pageSize + 1) + ':00',
+    uploadTime: (pageSize - i) + 'éve',
+    userName: 'user-' + term
+  }));
+  const matched = sampleVideos.filter(x => x.title.toLowerCase().includes(term.toLowerCase()));
   const json = {
     totalCount: totalCount,
     currentPage: page,
     previousPage: page > 1 ? (page - 1) : undefined,
     nextPage,
-    items: Array(remaining > pageSize ? pageSize : remaining).fill(0).map((x, i) => ({
-      title: 'Mock' + term + ' cím ' + (i + page * pageSize + 1),
-      href: '',
-      duration: (i + page * pageSize + 1) + ':00',
-      uploadTime: (pageSize - i) + 'éve',
-      userName: 'user-' + term
-    })),
+    items: [...matched, ...generatedItems.slice(0, pageSize - matched.length)],
   }
   return json;
 }
