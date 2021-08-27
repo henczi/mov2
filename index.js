adc009d50933e787ee0aca5a321f988f2a1fb0d9
Henczidai Bálint <henczidaibalint@gmail.com>
2021-08-27 13:04:09 +0200

    spa build, serve prod

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+import fs from 'fs';
+import sirv from 'sirv';
 import express from 'express';
+import compression from 'compression';
 
-const { PORT, NODE_ENV } = process.env;
+import api from './core/api.js';
+
+const { PORT = 3000, NODE_ENV = 'development' } = process.env;
 const dev = NODE_ENV === 'development';
 
+if (!fs.existsSync('web/build')) throw new Error('No web/build!');
+
 express()
-    .use('/api', require('core/api.js'))
-    // serve svelte-kit app
+    .use('/api', api)
+    .use(
+		compression({ threshold: 0 }),
+		sirv('web/build', { dev })
+	)
     .listen(PORT, err => {
         if (err) console.log('error', err);
     });
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 import api from '../core/api.js';
 
@@ -23,6 +24,8 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		ssr: false,
+		adapter: adapter(),
 		vite: {
 			plugins: [apiMiddlewarePlugin]
 		}
