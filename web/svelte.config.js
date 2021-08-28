939b55218a11fd3f11da39d37f2d638c00c26214
Henczidai Bálint <henczidaibalint@gmail.com>
2021-08-28 12:17:26 +0200

    dev - decodeuri

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -7,7 +7,7 @@ const apiMiddlewarePlugin = {
 	name: 'apiMiddlewarePlugin',
 	configureServer(server) {
 		server.middlewares.use('/api', (req, res, next) => {
-			req.query = (req.url.split('?')[1] || '').split('&').reduce((a, x) => (a[x.split('=')[0]] = x.split('=')[1], a), {});
+			req.query = (req.url.split('?')[1] || '').split('&').reduce((a, x) => (a[x.split('=')[0]] = decodeURIComponent(x.split('=')[1]), a), {});
 			res.json = function(obj) { this.end(JSON.stringify(obj)); };
 			next();
 		});
