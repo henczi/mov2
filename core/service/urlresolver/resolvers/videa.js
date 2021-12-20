009aeeb1adae9e9018b37c4a11dcec1431a129de
Henczidai Bálint <henczidaibalint@gmail.com>
2021-12-20 20:19:54 +0100

    videa video id fix

--- core/service/urlresolver/resolvers/videa.js
+++ core/service/urlresolver/resolvers/videa.js
@@ -4,7 +4,7 @@ const videa = require('../../videa/index.js');
 module.exports = class extends require('../resolver.js') {
   constructor() {
     super();
-    this.pattern = /^https?:\/\/((?:videa|videakid)\.hu)\/(?:player\/?\?v=|player\/v\/|videok\/)(?:.*-|)([0-9a-zA-Z]+)/;
+    this.pattern = /^https?:\/\/((?:videa|videakid)\.hu)\/(?:player\/?\?v=|player\/v\/|videok\/)(?:.*-|)([0-9a-zA-Z_]+)/;
   }
 
   async getVideoInfo({ mediaId }) {
